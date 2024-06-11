56bed6b3ad0debdae330b193fd1e35fccfd68cfe
Sumagna Das <sumagnadas@gmail.com>
2024-06-12 03:26:19 +0530

    Made the Stage Selection screen and Reordered the code

--- main.py
+++ main.py
@@ -1,44 +1,7 @@
 from kivy.app import App
-from kivy.graphics import Color, RoundedRectangle
-from pages import Page3
-from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
+from pages import Home, Page3, StageSel
+from kivy.uix.screenmanager import ScreenManager,NoTransition
 
-class Home(Screen):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-    def _btnchg(self, obj,i):
-        with obj.canvas:
-            App.get_running_app()._color[i] = Color(200/255, 200/255, 200/255,0.9)
-            App.get_running_app()._rect[i] = RoundedRectangle(pos=obj.pos, size=obj.size)
-    def on_b1(self, obj):
-        print("button1")
-        self.manager.current = "Page3"
-        self._btnchg(obj, 0)
-        print(obj)
-    def on_b2(self, obj):
-        print("button2")
-        self._btnchg(obj, 1)
-        print(obj)
-    def on_b3(self, obj):
-        print("button3")
-        self._btnchg(obj, 2)
-        print(obj)
-    def on_b4(self, obj):
-        print("button4a")
-        self._btnchg(obj, 3)
-        print(obj)
-    def on_b5(self, obj):
-        print("button4b")
-        self._btnchg(obj, 4)
-        print(obj)
-    def on_b6(self, obj):
-        print("button4c")
-        self._btnchg(obj, 5)
-        print(obj)
-    def on_set(self, obj):
-        print("settings")
-        self._btnchg(obj, 6)
-        print(obj)
 class MainApp(App):
     _color=dict()
     _rect = dict()
@@ -48,6 +11,7 @@ class MainApp(App):
         #self.settings_cls = SetingsWI
         self.sm.add_widget(Home(name="Home"))
         self.sm.add_widget(Page3(name="Page3"))
+        self.sm.add_widget(StageSel(name="StageSel"))
         self.rv = self.sm.get_screen('Page3').rv
         return self.sm
     def on_release(self, obj, i):
--- pages.py
+++ pages.py
@@ -10,10 +10,10 @@ from kivy.uix.recycleview import RecycleView
 from kivy.uix.recycleview.views import RecycleDataViewBehavior
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.textinput import TextInput
+from kivy.graphics import Color, RoundedRectangle
 from re import sub
 
 from datetime import datetime
-
 Builder.load_file("pages.kv")
 
 class NumericInput(TextInput):
@@ -36,9 +36,6 @@ class RallyRow(RecycleDataViewBehavior, BoxLayout):
     def on_enter(self):
         rv = App.get_running_app().rv
         rv.data[self.index]['carno'] = self.carno
-
-
-
 class RV(RecycleView):
     def __init__(self, **kwargs):
         super(RV, self).__init__(**kwargs)
@@ -65,10 +62,56 @@ class TimeLabel(Label):
         self.text = self.time
 class RoundedButton(Button):
     pass
+class Home(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+    def _btnchg(self, obj,i):
+        with obj.canvas:
+            App.get_running_app()._color[i] = Color(200/255, 200/255, 200/255,0.9)
+            App.get_running_app()._rect[i] = RoundedRectangle(pos=obj.pos, size=obj.size)
+    def on_b1(self, obj):
+        print("button1")
+        self.manager.current = "Page3"
+        self._btnchg(obj, 0)
+        print(obj)
+    def on_b2(self, obj):
+        print("button2")
+        self._btnchg(obj, 1)
+        print(obj)
+    def on_b3(self, obj):
+        print("button3")
+        self._btnchg(obj, 2)
+        print(obj)
+    def on_b4(self, obj):
+        print("button4a")
+        self._btnchg(obj, 3)
+        print(obj)
+    def on_b5(self, obj):
+        print("button4b")
+        self.manager.current = "StageSel"
+        self._btnchg(obj, 4)
+        print(obj)
+    def on_b6(self, obj):
+        print("button4c")
+        self._btnchg(obj, 5)
+        print(obj)
+    def on_set(self, obj):
+        print("settings")
+        self._btnchg(obj, 6)
+        print(obj)
 class SetPage(SettingsWithNoMenu):
     pass
-
+class StageSel(Screen):
+    day = ObjectProperty([])
+    day_no = ObjectProperty(None)
+    stage = ObjectProperty(None)
+    def on_day_no(self,instance,value):
+        print(self.day)
+        app = App.get_running_app()
+        if app.sm.current == 'StageSel':
+            print(self.day.index(self.day_no))
 class Page3(Screen):
     rv = ObjectProperty(None)
     def on_capture(self, time):
+        print(time)
         self.rv.data.append({'time': str(time),'carno': 0})
